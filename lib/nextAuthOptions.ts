1263d10158f09e66b35eba545d53aa6fe8d8e195
nwaxon2020 <princenwachukwu308@yahoo.com>
2025-05-02 13:01:40 +0100

    try

--- lib/nextAuthOptions.ts
+++ lib/nextAuthOptions.ts
@@ -58,5 +58,5 @@ export const authOptions: NextAuthOptions = {
     },
 
     secret: process.env.NEXTAUTH_SECRET,
-    debug: true, 
+
 }
--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from "next/server";
 
 export default async function middleware(req:NextRequest) {
-    const token = await getToken({req, secret: process.env.NEXTAUTH_SECRET}) 
+    const token = await getToken({req}) 
     const path = req.nextUrl.pathname;
 
     const routers = ["/","/trending","/contact","/videos"].includes(path)
